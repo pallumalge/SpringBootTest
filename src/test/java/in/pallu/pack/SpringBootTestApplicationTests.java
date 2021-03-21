2d236ad1a8e45f6fe42256f89609ced8580d91d6
91797 <91797@LAPTOP-1L3MP24M>
2021-03-21 22:15:08 +0530

    WH-8520 first time file update

---
+++ src/test/java/in/pallu/pack/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pallu.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
